70141d176a690c9c5b07fbf03e3e2608396f9869
sjlimiei25 <sjlim.iei.25@gmail.com>
2025-12-16 12:44:02 +0900

    타이타닉 데이터 인덱싱, 쿼리

--- 3_bulk_api.py
+++ 3_bulk_api.py
@@ -24,7 +24,7 @@ df = df.dropna()
 # ------------------------------------
 # 엘라스틱서치에 인덱싱
 # - 엘라스틱서치 연결
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, helpers
 
 try:
 
@@ -79,3 +79,56 @@ mapping = {
 #   - 인덱스 생성 : es.indices.create
 es.indices.create(index=INDEX_NAME, mappings=mapping)
 print(f'인덱스 생성 완료: {INDEX_NAME}')
+# --------------------------------------------------
+
+# 문서 추가 (Bulk API)
+# - 요청 시 데이터 타입 : dict
+# - 데이터셋으로부터 읽어온 데이터 타입 : DataFrame
+
+# DataFrame -> dict
+data_list = []
+for index, row in df.iterrows():    # iterrows() : (index, Series). 한 행씩 리턴
+  data = {
+    "_index": INDEX_NAME,
+    "_source": row.to_dict(),       # to_dict() : dict 타입으로 변환
+    "_id": row['PassengerId']       # _id 는 PassengerId 값으로 지정
+  }
+  data_list.append(data)
+
+# bulk api 사용
+success, error = helpers.bulk(es, data_list)
+print(f'문서 추가 완료. (성공: {success}, 실패: {error})')
+
+# * 추가 후 바로 검색하기 위해 refresh
+es.indices.refresh(index=INDEX_NAME)
+
+# * 여성 탑승객 정보만 조회 * ------------
+#   출력 예)
+#   - 이름: xxx, 나이: xx, 생존여부: 생존 또는 사망
+query = {
+  "query": {
+    "term": {
+      "Sex": "female"
+    }
+  },
+  "size": 30,
+  "from": 2
+}
+
+result = es.search(index=INDEX_NAME, body=query)
+print('* ---- 여성 탑승객 정보 ---- *')
+# print(result, type(result))
+print(f"= 검색결과 총 개수: {result['hits']['total']['value']}")
+
+passenger_list = result['hits']['hits']     # => 리스트 형태 []. 검색 결과 목록
+# print(type(passenger_list))
+for data in passenger_list:
+  name = data['_source']['Name']
+  age = data['_source']['Age']
+  # survived = "생존" if data['_source']['Survived'] == 1 else "사망"
+  if data['_source']['Survived'] == 1:
+    survived = "생존"
+  else:
+    survived = "사망"
+
+  print(f' - 이름: {name}, 나이: {age}, 생존여부: {survived}')
